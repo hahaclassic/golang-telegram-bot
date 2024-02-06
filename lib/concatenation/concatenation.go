2d5d4ddc24ea032a26ba80fffc7c977aa0d0a63a
hahaclassic <gavrilyukvlad221@gmail.com>
2024-02-07 00:06:51 +0300

    func EnumeratedJoinWithTags() was added

--- lib/concatenation/concatenation.go
+++ lib/concatenation/concatenation.go
@@ -5,7 +5,11 @@ import (
 	"strings"
 )
 
-// Создание пронумерованного списка в строке из списка строк
+// This function creates string like
+// ---------------------------------------
+// i. elements[i - 1] \n\n
+// ---------------------------------------
+// Where i - index of elements in the slice.
 func EnumeratedJoin(elements []string) string {
 
 	var enumerated strings.Builder
@@ -15,4 +19,38 @@ func EnumeratedJoin(elements []string) string {
 	}
 
 	return enumerated.String()
+}
+
+// This function creates string like
+// ---------------------------------------
+// i. tags[i - 1]
+// data[i - 1] \n\n
+// ---------------------------------------
+// Where i - index of elements in the slice.
+// len(data) must be equal to the len(tags), otherwise returns empty string
+//
+// If data[i] == tags[i], an empty string is written instead of tags[i]
+func EnumeratedJoinWithTags(data []string, tags []string) string {
+	if len(data) != len(tags) {
+		return ""
+	}
+	var enumerated strings.Builder
+
+	for i := 0; i < len(data); i++ {
+		if data[i] == tags[i] {
+			enumerated.WriteString(strconv.Itoa(i+1) + ".\n" + data[i] + "\n\n")
+		} else {
+			enumerated.WriteString(strconv.Itoa(i+1) + ". " + tags[i] + "\n" + data[i] + "\n\n")
+		}
+	}
+
+	return enumerated.String()
+}
+
+// returns a cropped string. an 3 dots are added at the end
+func trimData(data string, maxLen int) string {
+	if len(data) > maxLen {
+		data = data[:maxLen-3] + "..."
+	}
+	return data
 }
